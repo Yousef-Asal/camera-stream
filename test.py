eb8ea8ef7470c7bb6b65719d4bbde18db2910365
Yousef-Asal <yousefasal50@gmail.com>
2025-01-18 11:38:35 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,29 @@
-from http.server import SimpleHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+import io
+import picamera
+
+class MJPEGStreamHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Content-type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
+            with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
+                stream = io.BytesIO()
+                for _ in camera.capture_continuous(stream, format='jpeg', use_video_port=True):
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-Type', 'image/jpeg')
+                    self.send_header('Content-Length', len(stream.getvalue()))
+                    self.end_headers()
+                    self.wfile.write(stream.getvalue())
+                    stream.seek(0)
+                    stream.truncate()
+        else:
+            self.send_error(404)
+            self.end_headers()
 
 address = ('', 8000)
-httpd = HTTPServer(address, SimpleHTTPRequestHandler)
-print("Serving on port 8000...")
-httpd.serve_forever()
+server = HTTPServer(address, MJPEGStreamHandler)
+print("Streaming on port 8000...")
+server.serve_forever()
